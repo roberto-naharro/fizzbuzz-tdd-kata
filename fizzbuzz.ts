33f5bafdf73f68a95792e38dbf4706bde522d526
Roberto Naharro <roberto.naharro@gmail.com>
2020-01-16 20:07:16 +0100

    feat: FIZZBUZZ

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,8 +1,21 @@
+const checkDivideBy = (def: { [n: number]: string | number }) => (
+  checkN: number,
+): string | number =>
+  Object.keys(def)
+    .sort()
+    .reduce(
+      (prev: undefined | string, divisor) =>
+        checkN % Number(divisor) === 0
+          ? prev
+            ? (prev += def[divisor])
+            : def[divisor]
+          : prev,
+      undefined,
+    );
+
 export function fizzbuzz(n: number): string | number {
-  if (n % 3 === 0) {
-    return 'FIZZ';
-  }
-  if (n % 5 === 0) {
-    return 'BUZZ';
-  }
+  return checkDivideBy({
+    3: 'FIZZ',
+    5: 'BUZZ',
+  })(n);
 }
