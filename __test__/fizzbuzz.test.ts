ed30b5a133021174e6d94742717686a559f637d8
Roberto Naharro <roberto.naharro@gmail.com>
2020-01-16 19:45:27 +0100

    feat: BUZZ

--- __test__/fizzbuzz.test.ts
+++ __test__/fizzbuzz.test.ts
@@ -1,15 +1,21 @@
 import { fizzbuzz } from '../fizzbuzz';
 
 describe('fizzbuzz', () => {
+  const testFizzBuzz = (n, s: 'FIZZ' | 'BUZZ') => expect(fizzbuzz(n)).toBe(s);
+
   it('Si n mod 3 = 0 devuelve "FIZZ"', () => {
-    expect(fizzbuzz(3)).toBe('FIZZ');
-    expect(fizzbuzz(6)).toBe('FIZZ');
-    expect(fizzbuzz(63339)).toBe('FIZZ');
+    const testFizz = n => testFizzBuzz(n, 'FIZZ');
+
+    testFizz(3);
+    testFizz(6);
+    testFizz(63339);
   });
   it('Si n mod 5 = 0 devuelve "BUZZ"', () => {
-    expect(fizzbuzz(5)).toBe('BUZZ');
-    expect(fizzbuzz(10)).toBe('BUZZ');
-    expect(fizzbuzz(53242340)).toBe('BUZZ');
+    const testBuzz = n => testFizzBuzz(n, 'BUZZ');
+
+    testBuzz(5);
+    testBuzz(10);
+    testBuzz(53242340);
   });
   it.todo('Si n mod 3 = 0 y n mod 5 = 0 "FIZZBUZZ"');
   it.todo('si n no cumple las anteriores, devuelve n');
